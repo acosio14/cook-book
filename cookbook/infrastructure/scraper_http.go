d639eaef1160ef1f9ed436a021d61e359663bf94
Adrian Cosio <adriancosio14@gmail.com>
2026-03-22 06:26:56 -0500

    feat(WIP): fill in more scraper implementation code.

--- cookbook/infrastructure/scraper_http.go
+++ cookbook/infrastructure/scraper_http.go
@@ -1,17 +1,36 @@
 package infrastructure
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 type HTTPScraper struct {
-	httpClient http.Client
+	httpClient *http.Client
 }
 
 // Need to create a constructor
-func NewHTTPScraper() {
-
+func NewHTTPScraper() *HTTPScraper {
+	return &HTTPScraper{
+		httpClient: &http.Client{},
+	}
 }
 
-func (scraper HTTPScraper) Scrape(url string) (string, error) {
-
+func (scraper *HTTPScraper) Scrape(url string) (string, error) { //should string resp be a http.Respones? What type would that be?
+	//TO DO: Implementation Body
+	//1. Fetch URL, get raw HTML
+	resp, err := scraper.httpClient.Get(url)
+	if err != nil {
+		// handle request failure
+	}
+	defer resp.Body.Close()
+	//2. Extract desired content
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		// handle error
+	}
+	doc.Find()
+	//TO DO: real error handling
 	return "string", nil
 }
