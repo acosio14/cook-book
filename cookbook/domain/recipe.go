a0708c09915a04d3d386c770f86d9a2e00e4236a
Adrian Cosio <adriancosio14@gmail.com>
2026-03-23 06:49:19 -0500

    fix: rename notes to personal notes for clarity.

--- cookbook/domain/recipe.go
+++ cookbook/domain/recipe.go
@@ -1,10 +1,10 @@
 package domain
 
 type Recipe struct {
-	ID           int
-	URL          string
-	Name         string
-	Ingredients  []string
-	Instructions []string
-	Notes        []string
+	ID            int
+	URL           string
+	Name          string
+	Ingredients   []string
+	Instructions  []string
+	PersonalNotes []string
 }
