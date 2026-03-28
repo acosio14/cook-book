79ea89542a7707436188aca85006101ab59fb402
Adrian Cosio <adriancosio14@gmail.com>
2026-03-28 06:21:24 -0500

    chore: comment out unused code.

--- cookbook/service/service.go
+++ cookbook/service/service.go
@@ -17,3 +17,17 @@ func validateRecipe(*domain.Recipe) (bool, error) {
 func checkDuplicates(url string) (bool, error) {
 	return false, nil
 }
+
+// NOTE: Getting every possible schema from scrapper. Need to handle extra content and only
+// retrieve recipe.
+type RecipeSchema struct {
+	Name         string `json:"name"`
+	Ingredients  string `json:"recipeIngredient"`
+	Instructions string `json:"recipeInstruction"`
+}
+
+//var recipe RecipeSchema
+//json.Unmarshal([]byte(raw), &recipe)
+
+// Will recieve content from scraper and give it to AI with prompt to get exact recipe content.
+// maps AI response to Recipe Struct
