38f19b7cf75b1732de02ac3941b978d42ee53177
Adrian Cosio <adriancosio14@gmail.com>
2026-03-28 06:23:26 -0500

    feat: add sql initialization command.

--- cookbook/repository/storage_sqlite.go
+++ cookbook/repository/storage_sqlite.go
@@ -15,9 +15,26 @@ func NewRepository(path string) (*Repository, error) {
 	if err != nil {
 		return nil, err
 	}
-	db.Ping()
 
-	initTable()
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	initializeSQLTable := `
+	    CREATE TABLE IF NOT EXISTS Recipes (
+		    ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+			URL TEXT NOT NULL,
+			Name TEXT NOT NULL,
+			Ingredients TEXT NOT NULL,
+			Instructions TEXT NOT NULL,
+			Yield INTEGER,
+			Notes TEXT,
+		);
+	`
+	_, err = db.Exec(initializeSQLTable)
+	if err != nil {
+		return nil, err
+	}
 
 	return &Repository{db: db}, nil
 }
