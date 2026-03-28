2c0f72acb07a3738f9fa59bdf398a9e5c858dce8
Adrian Cosio <adriancosio14@gmail.com>
2026-03-28 06:22:05 -0500

    chore: rename interface methods to simplier versions.

--- cookbook/repository/storage.go
+++ cookbook/repository/storage.go
@@ -3,9 +3,9 @@ package repository
 import "github.com/acosio14/cook-book/cookbook/domain"
 
 type Storage interface {
-	CreateNewRecipe(*domain.Recipe) error
-	UpdateRecipe(*domain.Recipe) error
-	ReadRecipe(id int) (*domain.Recipe, error)
-	ReadAllRecipes() ([]*domain.Recipe, error)
-	DeleteRecipe(id int) error
+	Add(recipe *domain.Recipe) error
+	Update(recipe *domain.Recipe) error
+	ReadContent(recipe_id int) (*domain.Recipe, error)
+	List() ([]*domain.Recipe, error)
+	Delete(recipe_id []int) error
 }
